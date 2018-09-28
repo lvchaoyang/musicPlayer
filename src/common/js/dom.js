822525b5af449f4721c306e7ac0b7889d61a8987
lvchaoyang <960278791@qq.com>
2018-09-28 17:32:53 +0800

    recommend

---
+++ src/common/js/dom.js
@@ -0,0 +1,12 @@
+export function addClass(el, className) {
+  if (hasClassName(el, className)) {
+    return
+  }
+  let newClassName = el.className.split(',')
+  newClassName.push(className)
+  el.className = newClassName.join(' ')
+}
+export function hasClassName(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
