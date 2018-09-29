23675da09649858b0ed842ca77c3e7f237e4e468
lvchaoyang <960278791@qq.com>
2018-09-29 15:13:10 +0800

    add singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    歌手页面
+  <div class="singer">
+
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
 export default {
-  name: 'singer'
+  name: 'singer',
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data)
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+ .singer
+    position fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
